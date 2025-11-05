e9f7627e3448de438cbc4458b96e7a2243bce45f
Gusti-Rama <gustirama309@gmail.com>
2025-11-05 19:34:56 +0700

    databasenya kelupaan

--- database/vanish.sql
+++ database/vanish.sql
@@ -28,12 +28,13 @@ CREATE TABLE IF NOT EXISTS `file` (
   `uploaded_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `sender_id` int DEFAULT NULL,
   `receiver_id` int DEFAULT NULL,
+  `category` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   PRIMARY KEY (`id_file`),
   KEY `sender_id` (`sender_id`),
   KEY `receiver_id` (`receiver_id`),
   CONSTRAINT `file_ibfk_1` FOREIGN KEY (`sender_id`) REFERENCES `user` (`id_user`),
   CONSTRAINT `file_ibfk_2` FOREIGN KEY (`receiver_id`) REFERENCES `user` (`id_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Data exporting was unselected.
 
@@ -49,7 +50,7 @@ CREATE TABLE IF NOT EXISTS `text` (
   KEY `receiver_id` (`receiver_id`),
   CONSTRAINT `text_ibfk_1` FOREIGN KEY (`sender_id`) REFERENCES `user` (`id_user`),
   CONSTRAINT `text_ibfk_2` FOREIGN KEY (`receiver_id`) REFERENCES `user` (`id_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Data exporting was unselected.
 
@@ -61,7 +62,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `salt` binary(16) NOT NULL,
   PRIMARY KEY (`id_user`) USING BTREE,
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Data exporting was unselected.
 
